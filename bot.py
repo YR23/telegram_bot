4b6ffb92ee8eda4ff61927daad9b849d3cdb6545
YardenRise <yarden.rotem@risecodes.com>
2025-07-09 23:51:51 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,26 @@
+# api_key = '7975258773:AAFofm1W-hg4-OC1v-kcj0gB1Bz6rFlOqOg'
+
+import os
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+
+
+api_key = os.getenv("TELEGRAM_BOT_TOKEN", "7975258773:AAFofm1W-hg4-OC1v-kcj0gB1Bz6rFlOqOg")  # fallback for local test
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Welcome! What do you need?")
+
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text.lower().strip()
+    await update.message.reply_text(text)
+
+
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(api_key).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    app.run_polling()
+
+
